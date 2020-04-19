849dc5c5d3447f725791c9b933d79e51bfd9dfb9
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-04-19 19:35:29 +0500

    adding delete to object.js

--- basicjs/object.js
+++ basicjs/object.js
@@ -34,3 +34,18 @@ const object1 = {
 };
 console.log(Object.keys(object1));
 // expected output: Array ["a", "b", "c"]
+
+// The JavaScript delete operator removes a property from an object; if no more references to the same property are held, it is eventually released automatically.
+
+const Employee = {
+  firstname: "John",
+  lastname: "Doe",
+};
+
+console.log(Employee.firstname);
+// expected output: "John"
+
+delete Employee.firstname;
+
+console.log(Employee.firstname);
+// expected output: undefined
