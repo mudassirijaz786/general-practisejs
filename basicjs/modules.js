fbeb0708aecb1049dc0e42cd63c35d73f6403584
Mudassir Ijaz <ijazmudassir786@gmail.com>
2020-04-19 19:48:23 +0500

    updating module

--- basicjs/modules.js
+++ basicjs/modules.js
@@ -21,3 +21,17 @@ import { Square } from "./square";
 
 // We use Webpack to combine our JavaScript files into a
 // bundle.
+
+//so tips and tricks shortly
+
+// import defaultExport from "module-name";
+// import * as name from "module-name";
+// import { export1 } from "module-name";
+// import { export1 as alias1 } from "module-name";
+// import { export1 , export2 } from "module-name";
+// import { foo , bar } from "module-name/path/to/specific/un-exported/file";
+// import { export1 , export2 as alias2 , [...] } from "module-name";
+// import defaultExport, { export1 [ , [...] ] } from "module-name";
+// import defaultExport, * as name from "module-name";
+// import "module-name";
+// var promise = import("module-name");
